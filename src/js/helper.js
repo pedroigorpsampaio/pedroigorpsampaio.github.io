4897469dbf7e626576484c6c62b77403f0899027
BuildTools <unconfigured@null.spigotmc.org>
2021-04-26 17:33:27 -0300

    basic structure of project page implemented

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,15 +1,22 @@
 $(document).ready(function(){
+    var indexContent = $('#content').html();
+    
       $('#demo-carousel').carousel();
-       $('#demo-carousel2').carousel();
-       $('.materialboxed').materialbox();
+      $('#demo-carousel2').carousel();
+      $('.materialboxed').materialbox();
       $('.tooltipped').tooltip();
       
       $('#pucmonpage').click(function(e){
-          
             $('#content').load('pucmon.html');
-            e.preventDefault();
-        
-    });
+            e.preventDefault();        
+      });
+      
+      $('#2dmbpage').click(function(e){
+            alert(indexContent);
+            $('#content').html(indexContent);
+            e.preventDefault();        
+      });
+    
       
     });
  
